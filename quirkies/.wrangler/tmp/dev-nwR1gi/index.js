47d2881aaf4f7a515471d5e1c467388e54aac81f
Little Ollie <littleollienft@gmail.com>
2026-04-14 02:35:49 +0800

    j

--- quirkies/.wrangler/tmp/dev-nwR1gi/index.js
+++ quirkies/.wrangler/tmp/dev-nwR1gi/index.js
@@ -1525,7 +1525,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-TGXM0P/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-3MhYyD/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -1557,7 +1557,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-TGXM0P/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-3MhYyD/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
